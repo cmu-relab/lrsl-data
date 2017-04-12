3611ba2da1af67dd50b3cccddc763705adf844b7
Sudarshan Wadkar <git@wadkar.net>
2017-04-12 19:16:02 -0400

    Move code to src/lrsl/ prefix
    Move the data processing related code to lrsl.data.main()
    I am also trying out calling that main directly from root's run.py

--- run.py
+++ run.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from src.lrsl.data import get_state_laws, StateLaw
-
-def main():
-    state_laws_csv = './docs/state_laws.csv'
-    state_laws_dir = './docs'
-    state_laws = get_state_laws(state_laws_csv)
-    for state, law_filename in state_laws:
-        state_law = StateLaw(state, law_filename, state_laws_dir)
-        #break
+#from src.lrsl.data import get_state_laws, StateLaw, main
+import src.lrsl.data
 
 if __name__ == '__main__':
-    main()
+    src.lrsl.data.main()
+    #main()
--- src/lrsl/data.py
+++ src/lrsl/data.py
@@ -21,7 +21,7 @@ class StateLaw:
         #'—': '-',
         #'–': '-',
         #'‑': '-',
-        # Manual inspection is hard - use Unicode Character Classes
+        # Manual is error prone - use Unicode Character Classes
 
         # Unicode initial and final punctuation classes
         # See: https://www.compart.com/en/unicode/category/Pi
@@ -130,10 +130,17 @@ class StateLaw:
         return self.filename[:-4]
 
 def main():
-    state_laws = get_state_laws('./state_laws.csv')
+    state_laws_csv = './docs/state_laws.csv'
+    state_laws_dir = './docs'
+    state_laws = get_state_laws(state_laws_csv)
     for state, law_filename in state_laws:
-        state_law = StateLaw(state, law_filename)
+        state_law = StateLaw(state, law_filename, state_laws_dir)
         #break
+
+    #state_laws = get_state_laws('./state_laws.csv')
+    #for state, law_filename in state_laws:
+    #    state_law = StateLaw(state, law_filename)
+    #    #break
 
 def get_state_laws(state_laws_filename):
     with open(state_laws_filename, 'r') as state_laws_file:
